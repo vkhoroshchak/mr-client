d1fd0a51f530aa5551933572b4a52d21c3d10f51
mranch <anchiforov@gmail.com>
2021-11-28 03:09:11 +0200

    update custom_reducer

--- parsers/sql_parser.py
+++ parsers/sql_parser.py
@@ -393,8 +393,23 @@ def custom_reducer(parsed_sql, field_delimiter):  # noqa: C901
         """
             else:
                 return f"""
-    # data_frame = dd.read_csv(file_name, sep='{field_delimiter}')
+    # data_frame = dd.read_csv(file_name, sep='{field_delimiter}', encoding='latin-1')
     data_frame = dd.read_parquet(file_name, sep='{field_delimiter}')
+    # logger.info('include_headers: ' + str(include_headers))
+    # if include_headers:
+    #     data_frame = dd.read_parquet(file_name, sep='{field_delimiter}')
+    # else:
+    #     data_frame = dd.read_parquet(file_name, sep='{field_delimiter}', engine='pyarrow-dataset',
+    #                                 filters=[('key_column', '!=', 'key_column')])
+    # logger.info('before compute')
+    # logger.info(next(data_frame.compute()))
+    # logger.info(data_frame.loc[:, 'key_column'].compute())
+    # for x in data_frame[data_frame.columns[0]].compute():
+    #     logger.info(str(x))
+    # logger.info('after compute')
+    # logger.info('data_frame.key_column: ' + str(data_frame['key_column']))
+    # data_frame = data_frame[data_frame.key_column != 'key_column']
+    # data_frame = data_frame.drop(labels=[0])
         """
         except Exception as e:
             logger.info("Caught exception!" + str(e))
@@ -471,7 +486,7 @@ def custom_reducer(parsed_sql, field_delimiter):  # noqa: C901
 
     try:
         res = """
-def custom_reducer(file_name, dest):
+def custom_reducer(file_name, dest, include_headers=False):
     import dask.dataframe as dd
         """
 
@@ -486,7 +501,9 @@ def custom_reducer(file_name, dest):
         res += parse_orderby(parsed_sql.get("orderby"))
 
         res += f"""
-    data_frame.to_csv(dest, index=False, sep='{field_delimiter}')
+    # data_frame.to_csv(dest, index=False, sep='{field_delimiter}', single_file=False)
+    data_frame.to_csv(dest + '/' + os.path.splitext(os.path.basename(file_name))[0] + '.csv', index=False,
+                        sep='{field_delimiter}', single_file=True)
         """
 
         return res
